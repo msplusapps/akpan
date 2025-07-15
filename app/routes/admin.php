332871395f56e363b15f9a0276da475f9c58b72c
msplusapps <msplus247@gmail.com>
2025-07-15 12:14:50 +0100

    kk

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 // echo 'reading home route';
-Router::get("admin/", ['AdminController', 'index'])->middleware('auth');
+// Router::get("admin/", ['AdminController', 'index'])->middleware('auth');
 // Router::get('/about', ['WebController', 'about'])->name('about');
 // Router::get('/contact', ['WebController', 'contact'])->name('contact');
 
