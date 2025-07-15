8cc03a5edfeb802ec75bdd866e2017a5eb50a9af
msplusapps <msplus247@gmail.com>
2025-07-15 08:15:57 +0100

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'msplus247/akpan',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '8d0ca4b89b1d2811812bcc40daf548a2771fe729',
+        'reference' => 'a82b486db7a85eb511410802d860c265e0a480a0',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'msplus247/akpan' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '8d0ca4b89b1d2811812bcc40daf548a2771fe729',
+            'reference' => 'a82b486db7a85eb511410802d860c265e0a480a0',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
